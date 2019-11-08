7edf4aa12510d7342fb378e1c1f690a9eb59300f
Carine Ngwekwe <carine@Carines-MacBook-Pro.local>
2019-11-08 15:28:40 -0600

    python lesson

---
+++ quiz.py
@@ -0,0 +1,53 @@
+# example_5 = 10 +9
+#
+# print(example_5)
+# print ((4+5) * 3)
+#
+# ex_1 = 'this is a string'
+#
+# ex_2 = "this also a string"
+#
+# print(ex_1)
+# print(ex_2)
+#
+# ex_8 = "orange"
+# print(ex_8[3])
+# print('apple'[2])
+# print('sweetmother'[:3])
+# print('sweetmother'[4:6])
+# print('sweetmother'[2:7])
+# print('sweetmother'[5:])
+#
+# ex_3 = False
+# ex_4 = 0.456
+# ex_5 = 24
+# print(type(ex_3))
+# print(type(ex_4))
+# print(type(ex_5))
+#
+# ex_7 = str(True)
+# print(type(ex_7))
+
+# penne = 12 * 16.68
+# arrabiata_sauce = 6.98
+# garlic = 20 * 16.78
+# Seasoning = 15.26
+# artisan = 3.00
+# meatball = 12 * 4.39
+# items = (12 * 16.68 + 6.98 + 20*16.78 + 15.26 + 3.00 + 12*4.39)
+# print(items)
+# penne = 16.68
+# arrabiata = 6.98
+# garlic = 16.78
+# sauce = 6.98
+# seasoning = 15.78
+# artisan = 3.00
+# meatball = 4.39
+#
+# subtotal = (penne * 100 + arrabiata * 100 + garlic*100 + sauce*100 + seasoning*100 + artisan*100 + meatball*100 )/100
+# print(subtotal)
+print("**********\n ******\n *****\n ****\n **\n **\n *")
+
+name =input("enter name= ")
+print("your name is " + name + ".")
+print(name)
